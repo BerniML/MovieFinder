5f86c4993f11123cf9a2e8f84f30b0e58d71339d
Bernat Mayolas <bmayolas@gmail.com>
2019-10-15 20:09:44 +0200

    Skeleton
    Skeleton Vuejs + Vuex

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="loveToKnow">
+    <HomePage msg="Testing landing page"/>
+  </div>
+</template>
+
+<script>
+import HomePage from './components/HomePage.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HomePage
+  }
+}
+</script>
