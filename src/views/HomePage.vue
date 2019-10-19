d04e81e5bc7cccffe5dcf91a6c363eeae829e82f
Bernat Mayolas <bmayolas@gmail.com>
2019-10-19 11:52:20 +0200

    Include route paths

---
+++ src/views/HomePage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="HomePage">
+    <b-jumbotron>
+      <template v-slot:header>
+        <div class="row justify-content-center">MovieFinder</div>
+      </template>
+
+      <b-container>
+        <hr class="my-4" />
+        <div
+          class="row justify-content-center"
+        >Search app for searching movies and series, displaying the title, year, poster and director!</div>
+
+        <div class="row justify-content-center mt-3">
+          <HomeSearch />
+        </div>
+      </b-container>
+    </b-jumbotron>
+    <Recommendations />
+  </div>
+</template>
+
+<script>
+import HomeSearch from "@/components/HomeSearch.vue";
+import Recommendations from "@/components/Recommendations.vue";
+export default {
+  name: "HomePage",
+  components: {
+    HomeSearch,
+    Recommendations
+  }
+};
+</script>
---
+++ src/views/Movies.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="movieContainer" class="mt-5">
+    <b-container>
+      <div class="row justify-content-center">
+        <div class="col text-center">
+          <slider></slider>
+          <b-alert
+            class="mt-5 center-block"
+            v-show="search.emptyResults"
+            show
+            variant="warning"
+          >I'm sorry, no movies found with this title, please try again!</b-alert>
+        </div>
+      </div>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import Slider from '@/components/Slider.vue'
+
+export default {
+  name: 'Movies',
+  components: {
+    Slider
+  },
+  computed: {
+    ...mapState(['movieQuery', 'search'])
+  },
+  mounted () {
+    // If the user did not perform  any search
+    if (!this.movieQuery) {
+      this.$router.push({ name: 'home' })
+    }
+  }
+}
+</script>
+<style scoped>
+.alert {
+  width: 50%;
+  display: inline-block;
+  margin: 0 auto;
+}
+</style>
